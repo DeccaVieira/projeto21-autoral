b4c6c4ec352e4a016b46e59650716a444cc7a60a
Andreia Vieira <andreiavieira2353@gmail.com>
2023-03-22 20:58:51 -0300

    fix bug

--- src/schemas/users-schemas.ts
+++ src/schemas/users-schemas.ts
@@ -2,12 +2,12 @@ import Joi from "../../node_modules/joi/lib/index.js";
 import { Users } from "../protocols/signUp-protocols.js";
 
 const signUpSchema = Joi.object <Users>({
-  cpf: Joi.string().required().min(11),
+  cpf: Joi.string().required(),
   name: Joi.string().required().min(2).max(50),
   email: Joi.string().email().required().min(3).max(50),
   password: Joi.string().required().min(6),
   confirmPassword: Joi.ref("password"),
-  phoneNumber: Joi.string().required().min(9)
+  phoneNumber: Joi.string().required()
 });
 
 export default signUpSchema;
